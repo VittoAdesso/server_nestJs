b179a5d78474e05e17e1cbff4c6824e98d035155
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-29 18:41:31 +0200

    included CRUD / decorators

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    getTask() : string {
+            return 'Return Tasks';
+        }
+
+    @Post()
+    createTask(@Body() task): string {
+        return 'Creating task'
+    }
+
+    @Put()
+    updateTask() : string {
+        return 'Updating Task';
+    }
+
+    @Delete()
+    deleteTask() : string {
+        return 'Deleting Task';
+    }
+}
