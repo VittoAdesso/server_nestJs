a4c04ecf2a3c86f631ec5a14bc6577caee8de46b
Vittorio Adesso <adessovittorio8@gmail.com>
2022-08-30 09:43:08 +0200

    create interface & use

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './interfaces/Task'; 
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+
+    tasks: Task[] = [
+        {
+            id: 1,
+            title: 'Testing',
+            description: 'test desc ',
+            done: true,
+        }, 
+        {
+            id: 2,
+            title: 'Testings',
+            description: 'test all',
+            done: false,
+        }
+    ];
+}
